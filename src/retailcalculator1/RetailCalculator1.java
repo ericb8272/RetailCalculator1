d9f48c0825ff201281f7eab63f6ab5d5235b9dcc
Eric Benedict <ericb8272@yahoo.com>
2014-05-15 16:12:14 -0500

    Initial Commit

---
+++ src/retailcalculator1/RetailCalculator1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package retailcalculator1;
+
+/**
+ *
+ * @author macuser
+ */
+public class RetailCalculator1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
